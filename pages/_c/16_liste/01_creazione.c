bcc0067f9feabea46fda09df64787ee441cc673a
Carmelo Castiglione <carmelo.c.castiglione@gmail.com>
2024-05-04 22:24:32 +0200

    Allocazione dinamica della memoria

---
+++ pages/_c/16_liste/01_creazione.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_node {
+    int val;
+    struct s_node * next; // puntatore al prossimo elemento
+} node;
+
+int main() {
+    // Definisco la testa della lista
+    node * head = NULL;
+    head = (node *) malloc(sizeof(node));
+    if (head == NULL) {
+        return 1;
+    }
+    // Inserisco dei valori
+    head->val = 1;
+    head->next = NULL;
+
+    // Creo un altro elemento
+    node * elem = NULL;
+    elem = (node *) malloc(sizeof(node));
+    if (head == NULL) {
+        return 1;
+    }
+    // Inserisco dei valori
+    elem->val = 2;
+    elem->next = NULL;
+    // Collego i due elementi
+    head->next = elem;
+
+    return 0;
+}
